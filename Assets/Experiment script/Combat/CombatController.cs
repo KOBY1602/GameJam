aa4ee256e155471a80e8c0d5cc87452795f9ef15
KOBY1602 <150656547+KOBY1602@users.noreply.github.com>
2024-06-08 22:10:42 +0700

    anim fail

--- Assets/Experiment script/Combat/CombatController.cs
+++ Assets/Experiment script/Combat/CombatController.cs
@@ -14,8 +14,8 @@ public class CombatController : MonoBehaviour
 
     public FirstPersonController firstPersonController;
     public FaceTarget FaceTarget;
-    public Animation AnimationParent;
-
+    public GameObject animationParent;
+    private Animator animator;
 
     // Start is called before the first frame update
 
@@ -32,8 +32,7 @@ public class CombatController : MonoBehaviour
         PlacementSystem.SetActive(false);
         Plane.SetActive(false);
         firstPersonController = FindObjectOfType<FirstPersonController>();
-
-        AnimationParent = gameObject.GetComponent<Animation>();
+        animator = animationParent.GetComponent<Animator>();
         // Initialize last mouse position
         lastMousePosition = Input.mousePosition;
     }
@@ -69,7 +68,7 @@ public class CombatController : MonoBehaviour
             InputManager.SetActive(false);
             PlacementSystem.SetActive(false);
             Plane.SetActive(false);
-            AnimationParent.Play("Reload1");
+            animator.Play("Reload1");
 
         }
         //else if (Input.GetKeyDown(KeyCode.)
