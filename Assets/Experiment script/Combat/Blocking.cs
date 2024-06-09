e4a4ea0b060451a36565123fb5ba3a46a2885926
KOBY1602 <150656547+KOBY1602@users.noreply.github.com>
2024-06-09 12:16:48 +0700

    blocking completed

--- Assets/Experiment script/Combat/Blocking.cs
+++ Assets/Experiment script/Combat/Blocking.cs
@@ -6,7 +6,7 @@ public class Blocking : MonoBehaviour
 {
     
     
-    //public GameObject hitEffectPrefab;
+    public GameObject hitEffectPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +22,14 @@ public class Blocking : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
+        
         // Check if the collision is with a target object
-        //if (collision.gameObject.CompareTag("Projectile") && GetComponent<CombatController>().isDefending)
+        if (collision.collider.gameObject.CompareTag("Sword"))
         {
-           // Debug.Log("Hit");
-            //GameObject hitEffect = Instantiate(hitEffectPrefab, collision.transform.position, Quaternion.identity);
-            // destroy the projectile on collision
-           // Destroy(collision.gameObject);
+            Debug.Log("Hit");
+            GameObject hitEffect = Instantiate(hitEffectPrefab, collision.transform.position, Quaternion.identity);
+             //destroy the projectile on collision
+           Destroy(gameObject);
         }
     }
 }
